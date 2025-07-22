ab022ca2d6a0079aeeff03bba5e555a4ff4040c0
FernandoPerez233 <al45722@un.edu.mx>
2025-07-22 16:27:23 -0600

    img and js

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// MENU RESPONSIVE
+var btnMenuOpen = document.getElementById("btnMenuOpen");
+var btnMenuClose = document.getElementById("btnMenuClose");
+var menubar =document.getElementById("menubar");
+
+btnMenuOpen.addEventListener('click',function()
+    {menubar.classList.add('active');
+
+ });
+
+ btnMenuClose.addEventListener('click',function()
+    {menubar.classList.remove('active');
+         });
+
+var enlaces = document.getElementById("enlaces");
+enlaces.addEventListener('click',function()
+ {
+    menubar.style.transitionDelay = '0.5s';
+    menubar.classList.remove('active');
+ });
+
+  var container = document.querySelector('.slider');
+ var btnIzq = document.getElementById('btn-izquierda');{
+     
+ }
+ 
